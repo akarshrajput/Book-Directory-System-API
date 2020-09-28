1370ea10dc6c5e412af16ec55d8c91608a5da6d2
Stanyke <Chibuike.stanley50@gmail.com>
2020-09-28 09:36:39 +0100

    Used CORS npm

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) =>
 {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 	
 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     
