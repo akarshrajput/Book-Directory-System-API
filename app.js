9400519052f376d5ec7b33fd3d4557ff653bb7d9
Stanyke <Chibuike.stanley50@gmail.com>
2020-09-28 15:41:30 +0100

    Used CORS npm

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-​
+
 const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use(cors());
-
+app.use((req, res, next) =>
+{
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+	
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	next();
+});
 
 
 app.use(bodyParser.json({ limit: '100mb' }));
