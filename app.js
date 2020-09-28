5df54ae7f4673e2d3287202e310950651dfae307
Stanyke <Chibuike.stanley50@gmail.com>
2020-09-28 09:03:09 +0100

    Removed CORS npm

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use(cors())
+app.use((req, res, next) =>
+{
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    
+    res.setHeader('Access-Control-Allow-Credentials', true)
+
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	next();
+});
 
 
 app.use(bodyParser.json({ limit: '100mb' }));
