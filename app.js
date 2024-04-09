a72041e7d486db75716a5b2df13c44574ef9a10f
Akarsh Rajput <112371948+akarshrajput@users.noreply.github.com>
2024-04-09 15:29:18 +0530

    Initial comjit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
-app.use(express.static('public'));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.static("public"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,22 +1,26 @@
-const app = require('./app');
-const mongoose = require('mongoose');
+const app = require("./app");
+const mongoose = require("mongoose");
 
-const bookRoute = require('./routes/book');
+const bookRoute = require("./routes/book");
 
-const db = "mongodb+srv://stanyke:CKj8n66FFnWsxWr9@cluster0.5seyi.mongodb.net/book-directory-system-api?retryWrites=true&w=majority";
+const db =
+  "mongodb+srv://shashank:6X!r!Z5b-3SikA@@book-api-cluster-1.kkuebtk.mongodb.net/book-api?retryWrites=true&w=majority&appName=book-api-cluster-1";
 
-mongoose.connect(db, {
+mongoose
+  .connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
-}).then(() => {
-    console.log(`MongodDB's mongoose is connected`)
-}).catch((err) => {
-    console.log(`MongodDB's mongoose failed to connect`)
-})
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log(`MongodDB's mongoose is connected`);
+  })
+  .catch((err) => {
+    console.log(`MongodDB's mongoose failed to connect`);
+  });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
